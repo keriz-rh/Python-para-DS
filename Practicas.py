9901202179f37136cab6c912de06e6d52639394f
Kevin Rivera <114261500+keriz-rh@users.noreply.github.com>
2023-10-25 19:45:47 -0600

    Create Practicas.py

---
+++ Practicas.py
@@ -0,0 +1,21 @@
+Parametros
+[ ]
+Nombre = 'Andrea'
+[ ]
+def Saludar_con_parametro(nombre) :
+  print(f'Hola {Nombre} es un gusto')
+[ ]
+Saludar_con_parametro(Nombre)
+output
+Hola Andrea es un gusto
+[ ]
+def Velocidad(distancia, tiempo) :
+  v = distancia / tiempo ;
+  print(f'Velocidad {v} m/s');
+
+
+[ ]
+Velocidad(100,20)
+output
+Velocidad 5.0 m/s
+[ ]
